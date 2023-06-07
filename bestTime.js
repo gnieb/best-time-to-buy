8ad7a40976f07293f71a670d7d2aedde9d634778
Grace Nieboer <grace.nieboer@outlook.com>
2023-06-06 23:58:06 -0400

    set up test file and main algorithm file

--- bestTime.js
+++ bestTime.js
@@ -1,11 +1,11 @@
-const bestTime = require('../bestTime.js');
-const expect = require('chai').expect;
-
-describe('Testing the best time to buy stock algorithm problem from leetcode', function() {
-    it('It can handle an empty array', function(done){
-        expect(bestTime([])).to.equal(0);
-        done();
-    })
+function bestTimeForStock (arr) {
 
 
-})
+} 
+
+if (require.main == module) {
+
+    console.log("")
+}
+
+module.exports = bestTimeForStock;
