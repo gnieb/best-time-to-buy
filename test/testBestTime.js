b473307e0d081e63426e941e75a2dddcb71a66b7
Grace Nieboer <grace.nieboer@outlook.com>
2023-06-11 22:35:20 -0400

    test added with no profit and test to ensure a profit

--- test/testBestTime.js
+++ test/testBestTime.js
@@ -6,6 +6,14 @@ describe('Testing the best time to buy stock algorithm problem from leetcode', f
         expect(bestTime([])).to.equal(0);
         done();
     })
+    it('The algorithm handles an array in which there is no profit possible', function(done) {
+        expect(bestTime([7,6,5,4])).to.equal(0);
+        done();
+    })
+    it('The algorithm handles an array in which a profit can be made', function(done){
+        expect(bestTime([1,4,7])).to.equal(6);
+        done();
+    })
 
 
 })
