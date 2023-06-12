6ddb64c517e1ec74a709060861865a3fde93bf61
Grace Nieboer <grace.nieboer@outlook.com>
2023-06-11 22:29:54 -0400

    algorithm completed and test number 1 passing

--- bestTime.js
+++ bestTime.js
@@ -1,11 +1,32 @@
 function bestTimeForStock (arr) {
-
+    let profit = 0
+    
+    while (arr.length > 1) {
+        if (  Math.max(arr[1], arr[arr.length - 1]) - arr[0] > profit ) {
+            profit =  Math.max(arr[1], arr[arr.length - 1]) - arr[0]
+            arr = arr.slice(1)
+            console.log(arr)
+        } else {
+            arr = arr.slice(1)
+            console.log("no profit this time", arr)
+        }
+    }
+console.log(profit,arr)
+    return profit
+    
 
 } 
 
 if (require.main == module) {
 
-    console.log("")
+    console.log("Expecting: 6")
+    console.log("=>", bestTimeForStock([3,4,2,1,7]))
 }
 
 module.exports = bestTimeForStock;
+
+//  take the max difference between the arr[positionX] and the maximumof the other 
+// integers
+// go to mthe next position. if greater than the profit before, replace the profit value with the difference and keep moving
+
+
